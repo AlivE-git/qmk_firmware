eaa345f045f2eededd5bb9057db5e67828180f58
AlivE-git <pavel-741@yandex.ru>
2023-09-24 23:47:14 +0300

    my oled config

---
+++ keyboards/ymdk/melody96/keymaps/AlivE-git/rules.mk
@@ -0,0 +1,17 @@
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+# OLED_DRIVER_ENABLE = yes
+OLED_TRANSPORT = i2c
+OLED_DRIVER = ssd1306
+WPM_ENABLE = yes
+
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
